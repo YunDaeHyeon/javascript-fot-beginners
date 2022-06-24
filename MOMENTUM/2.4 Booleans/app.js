cbc3959cc111d5c2d99d6869fa00009998227de0
DaeHyeon <kayuaao96@gmail.com>
2022-06-24 14:47:15 +0900

    22.06.26 Chapter 2.4 Booleans

---
+++ MOMENTUM/2.4 Booleans/app.js
@@ -0,0 +1,9 @@
+const amIFat = false;
+let somthing;
+console.log(somthing, amIFat);
+
+// undefined = 값이 정이되지 않았을 때
+// null  = 값 X
+// 자바에서는 할당된 변수에 값이 존재하지 않을 시 NPE가 발생하거나 null로
+// 채워지지만 JS에서는 메모리에 할당된 변수가 아무런 값을 가지고 있지 않을 시
+// undefined가 출력된다.
