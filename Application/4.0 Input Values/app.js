e26a1c7cd89c1a86a00daffc4e99b3d2ec1e9096
DaeHyeon <kayuaao96@gmail.com>
2022-07-06 19:25:22 +0900

    22.07.06 Input Values update

--- Application/4.0 Input Values/app.js
+++ Application/4.0 Input Values/app.js
@@ -1,17 +1,12 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-// 유저 이름 유효성 검사
+// 유저 이름 유효성 검사 - 
 // 1. 너무 길지 않을 것
 // 2. 비어있지 않을 것
 function onLoginButtonClick(){
     const userName = loginInput.value; // input의 value 가져오기
-    if(value === ""){ // value가 비어있다면
-        alert("Please write your name");
-    }else if(userName.length > 15){ // 유저의 이름이 15자가 넘으면
-        alert("Your name is too long.");
-    }
-    
+    console.log(userName);
 }
 
 loginButton.addEventListener("click", onLoginButtonClick);
