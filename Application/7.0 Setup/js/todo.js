066aecd0493800a2ba330e7d72195770da2aa0e2
DaeHyeon <kayuaao96@gmail.com>
2022-07-08 23:16:29 +0900

    22.07.08 todo list setup

---
+++ Application/7.0 Setup/js/todo.js
@@ -0,0 +1,10 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+const toDoInput = document.querySelector("#todo-form input");
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    toDoInput.value = ""; // 사용자가 todo 입력칸에 엔터를 칠 때마다 칸 비우기
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
