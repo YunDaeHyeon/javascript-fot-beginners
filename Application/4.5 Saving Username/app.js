c882058d821c8fa398c2bea8387386842be163d9
DaeHyeon <kayuaao96@gmail.com>
2022-07-08 18:54:36 +0900

    22.07.08 Loding UserName

--- Application/4.5 Saving Username/app.js
+++ Application/4.5 Saving Username/app.js
@@ -8,9 +8,9 @@ function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add("hidden"); // 로그인 입력 form에 .hidden을 추가하여 화면에서 사라지게 만듬.
     const userName = loginInput.value;
+    localStorage.setItem("userName",userName); // 사용자 이름 LS에 저장
     greeting.innerText = `Hello ${userName}`; // #greeting에 텍스트 추가 ``에 ${}을 활용하여 변수 서식문자 지정
     greeting.classList.remove(HIDDEN_CLASSNAME); // #greeting의 초기값은 hidden이므로 form에서 입력받았을 시 삭제
-    localStorage.setItem("userName",userName); // 사용자 이름 LS에 저장
 }
 
 /*
